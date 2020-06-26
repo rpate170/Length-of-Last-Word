46aef0b72eae2c1b157cc865b3ae6737224f9db8
rpate170 <rushi.patel175@gmail.com>
2020-06-25 21:12:48 -0400

    Leetcode problem 58

---
+++ lengthLastWord.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        String result = s.trim();
+        int count=0;
+        for (int i = result.length()-1; i >= 0; i--) {
+            char c = result.charAt(i);
+            if (c == ' '){
+                return count;
+            }
+            else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
